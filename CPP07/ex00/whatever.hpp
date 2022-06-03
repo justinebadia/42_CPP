7156c2d86e075bbcaefc0a0a17bd3248c2dfbef6
Justine Badia <jbadia@c1r7p4.42quebec.com>
2022-06-03 15:44:36 -0400

    cpp07

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbadia <jbadia@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/03 15:20:26 by jbadia            #+#    #+#             */
+/*   Updated: 2022/06/03 15:37:14 by jbadia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+template <typename T>
+
+T const &swap(const T &a, const T &b);
+T const &min(const T &a, const T &b);
+T const &max(const T &a, const T &b);
+
+#endif
